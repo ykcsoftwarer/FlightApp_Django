7d62f854be34efeee6e7183ed5c359490d766c92
ykcsoftwarer <yusufkucukcopur@gmail.com>
2023-01-04 20:38:50 +0300

    Flight App 2.part start

---
+++ README.md
@@ -0,0 +1,6 @@
+BRD(Business requirements documents) -  ERD(Entity relationship diagram)
+Swagger(Document + Test)
+Debug_Toolbar
+Logger
+PostgreSQL
+dev-prod env
---
+++ main/settings/__init__.py
@@ -0,0 +1,12 @@
+#* __init__.py dosyası içine;
+from .base import *
+
+env_name = config("ENV_NAME")
+
+if env_name == "prod":
+  from .prod import *
+  
+elif env_name == "dev":
+  from .dev import *
+
+  
